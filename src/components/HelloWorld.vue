957cc75f245b7ef2f040957b7f17fecce1dcda07
Daisuke Kondo <gim0kondo@gmail.com>
2019-04-12 02:10:48 +0900

    feat: make sound box base

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <div v-for="name in names">
+      <SoundBox :name="name"/>
+    </div>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -33,10 +36,20 @@
 </template>
 
 <script>
+import SoundBox from '@/components/SoundBox.vue'
+
 export default {
   name: 'HelloWorld',
+  components: {
+    SoundBox
+  },
   props: {
     msg: String
+  },
+  data () {
+    return {
+      names: ['aaaa', 'bbbb', 'cc']
+    }
   }
 }
 </script>
---
+++ src/components/SoundBox.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sound-box">
+    <div>曲名:</div>{{name}}<br/>
+    <div>メモ</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SoundBox',
+  props: {
+    name: String
+  }
+}
+</script>
