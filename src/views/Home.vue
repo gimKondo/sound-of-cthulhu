3d40666cea861258e2d8a2f6be8b35f3b3b43dd0
Kondo Daisuke <gim0kondo@gmail.com>
2019-04-12 16:28:01 +0900

    fix: broken Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,19 +3,9 @@
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
-</script>
-ias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '../components/HelloWorld'
 
 export default {
-  name: 'home',
   components: {
     HelloWorld
   }
