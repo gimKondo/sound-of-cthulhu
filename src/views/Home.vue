ff0b4c86d8a71c750e803e21aff5f5f98bb1cb38
Kondo Daisuke <gim0kondo@gmail.com>
2019-04-12 12:34:11 +0900

    feat: introduce Vuetify as a design framework

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
-// @ is an alias to /src
+  import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    components: {
+      HelloWorld
+    }
+  }
+</script>
+ias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
