fdc7b7e42ddf02c554e6a9e11ced0f20f47b46b3
Kondo Daisuke <gim0kondo@gmail.com>
2019-04-15 17:16:31 +0900

    feat: remove default Home

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -8,8 +7,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'SoundList',
+      component: () => import('./views/SoundList.vue')
     },
     {
       path: '/about',
@@ -18,11 +17,6 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path: '/sound-list',
-      name: 'SoundList',
-      component: () => import('./views/SoundList.vue')
     }
   ]
 })
