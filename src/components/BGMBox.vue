a9593d59d668daca327db572be6c31e98a7418d2
gim_kondo <gim0kondo@gmail.com>
2019-04-27 17:14:14 +0900

    Feat/play sound (#6)

--- src/components/BGMBox.vue
+++ src/components/BGMBox.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bgm-box">
-    <v-card>
+    <v-card v-on:click="playSound">
       <v-card-title class="font-weight-bold bgm-box-title">{{name}}</v-card-title>
       <div>メモ</div>
     </v-card>
@@ -12,6 +12,25 @@ export default {
   name: 'BGMBox',
   props: {
     name: String
+  },
+  methods: {
+    playSound: function () {
+      alert('playing sound: ' + this.name)
+      const context = new AudioContext()
+      const electron = require('electron')
+      const fs = electron.remote.require('fs')
+      const toArrayBuffer = function (buf) {
+        return buf.buffer.slice(buf.byteOffset, buf.byteOffset + buf.byteLength)
+      }
+      fs.readFile('public/bgm_sample.mp3', (err, data) => {
+        context.decodeAudioData(toArrayBuffer(data), function (buffer) {
+          const source = context.createBufferSource()
+          source.buffer = buffer
+          source.connect(context.destination)
+          source.start(0)
+        }).then()
+      })
+    }
   }
 }
 </script>
