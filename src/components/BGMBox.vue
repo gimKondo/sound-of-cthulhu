31bcd14856b7203428049b238c9110323ba6f808
u-ishii <42168424+u-ishii@users.noreply.github.com>
2019-04-27 18:27:45 +0900

    Feat/play pause (#8)
    * feat: define `playing`
    
    * feat: initialize on created
    
    * feat: suspend and resume sound
    
    * feat: change for review

--- src/components/BGMBox.vue
+++ src/components/BGMBox.vue
@@ -1,35 +1,60 @@
 <template>
   <div class="bgm-box">
-    <v-card v-on:click="playSound">
+    <v-card>
       <v-card-title class="font-weight-bold bgm-box-title">{{name}}</v-card-title>
       <div>メモ</div>
+      <v-icon @click="playSound" v-if="!playing">play_arrow</v-icon>
+      <v-icon @click="pauseSound" v-else>pause</v-icon>
     </v-card>
   </div>
 </template>
 
 <script>
+const context = new AudioContext()
+const electron = require('electron')
+const fs = electron.remote.require('fs')
 export default {
   name: 'BGMBox',
   props: {
     name: String
   },
-  methods: {
-    playSound: function () {
-      alert('playing sound: ' + this.name)
-      const context = new AudioContext()
-      const electron = require('electron')
-      const fs = electron.remote.require('fs')
-      const toArrayBuffer = function (buf) {
-        return buf.buffer.slice(buf.byteOffset, buf.byteOffset + buf.byteLength)
+  data () {
+    return {
+      source: null,
+      isStarted: false,
+      isPlaying: false
+    }
+  },
+  created () {
+    fs.readFile('public/bgm_sample.mp3', (error, data) => {
+      if (error) {
+        console.error(error)
       }
-      fs.readFile('public/bgm_sample.mp3', (err, data) => {
-        context.decodeAudioData(toArrayBuffer(data), function (buffer) {
-          const source = context.createBufferSource()
-          source.buffer = buffer
-          source.connect(context.destination)
-          source.start(0)
-        }).then()
-      })
+      const arraySoundBuffer = data.buffer.slice(data.byteOffset, data.byteOffset + data.byteLength)
+      context.decodeAudioData(arraySoundBuffer, (decodedSoundBuffer) => {
+        const source = context.createBufferSource()
+        source.buffer = decodedSoundBuffer
+        source.connect(context.destination)
+        this.source = source
+      }).then()
+    })
+  },
+  methods: {
+    playSound () {
+      if (!this.source) {
+        return
+      }
+      if (this.started) {
+        context.resume().then()
+      } else {
+        this.source.start(0)
+        this.started = true
+      }
+      this.playing = true
+    },
+    pauseSound () {
+      context.suspend().then()
+      this.playing = false
     }
   }
 }
