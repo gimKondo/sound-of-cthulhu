25141018b5c2b85e20117548a6df0317c4fd5325
u-ishii <yuki.ishii.business@gmail.com>
2019-05-10 16:51:55 +0900

    feat: split BGMBox components

---
+++ src/components/RemoveSound.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-icon @click="$emit('remove-sound')" :color="color">{{text}}</v-icon>
+</template>
+
+<script>
+export default {
+  name: 'RemoveSound',
+  props: {
+    text: String,
+    color: String
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/SoundBoxTitle.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card-title class="font-weight-bold" :class="color">{{text}}</v-card-title>
+</template>
+
+<script>
+
+export default {
+  name: 'SoundBoxTitle',
+  props: {
+    text: String,
+    color: String
+  }
+}
+</script>
+
+<style>
+.v-card-title {
+  word-wrap: break-word;
+}
+</style>
