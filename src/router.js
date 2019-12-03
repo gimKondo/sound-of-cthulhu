4c9919168d17a04a4c0e7939ab4fe4e67c7038f8
Daisuke Kondo <gim0kondo@gmail.com>
2019-12-04 01:23:38 +0900

    :shower: remove about page

--- src/router.js
+++ src/router.js
@@ -9,14 +9,6 @@ export default new Router({
       path: '/',
       name: 'Main',
       component: () => import('./views/Main.vue')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
