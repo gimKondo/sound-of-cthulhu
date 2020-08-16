f4d513fa3d503b19445d7a42a4e77931f73090f6
Kondo Daisuke <gim0kondo@gmail.com>
2020-08-17 00:40:51 +0900

    :shirt: fix lint error on test code

--- tests/unit/services/DiscordMsg.spec.js
+++ tests/unit/services/DiscordMsg.spec.js
@@ -2,15 +2,15 @@ import * as DiscordMsg from '../../../src/services/DiscordMsg'
 
 describe('DiscordMsg', () => {
   describe.each`
-    type         | expected                       | msg
-    ${'join'}    | ${DiscordMsg.MSG_TYPE_JOIN}    | ${':soc: join'}
-    ${'join'}    | ${DiscordMsg.MSG_TYPE_JOIN}    | ${'<:soc:12334244343> join'}
-    ${'leave'}   | ${DiscordMsg.MSG_TYPE_LEAVE}   | ${':soc: leave'}
-    ${'invalid'} | ${DiscordMsg.MSG_TYPE_INVALID} | ${'test'}
-    `('judgeType', ({ type, expected, msg }) => {
-    test(`${type}("${msg}")`, () => {
-      const type = DiscordMsg.judgeType(msg)
-      expect(type).toBe(expected)
-    })
+  type         | expected                       | msg
+  ${'join'}    | ${DiscordMsg.MSG_TYPE_JOIN}    | ${':soc: join'}
+  ${'join'}    | ${DiscordMsg.MSG_TYPE_JOIN}    | ${'<:soc:12334244343> join'}
+  ${'leave'}   | ${DiscordMsg.MSG_TYPE_LEAVE}   | ${':soc: leave'}
+  ${'invalid'} | ${DiscordMsg.MSG_TYPE_INVALID} | ${'test'}
+  `('judgeType', ({ type, expected, msg }) => {
+  test(`${type}("${msg}")`, () => {
+    const type = DiscordMsg.judgeType(msg)
+    expect(type).toBe(expected)
   })
+})
 })
